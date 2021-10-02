403ded70c6c5330d8d58452b3fe59f39fd782738
Arya <aapt1720@gmail.com>
2021-10-02 14:05:24 +0330

    open source version of old repeated

--- repeated.py
+++ repeated.py
@@ -1,5 +1,6 @@
 # Get text-file path from USER
-print('Enter Absolute Path\n[if text is in current path, jusu enter the name]')
+print('Enter Absolute Path')
+print('[if text is in current path, jusu enter the name]')
 filePath = input('file path: ')
 
 with open("junks.txt") as f:
@@ -9,37 +10,40 @@ junks = []
 for j in content:
     junks.append(j.strip('\n'))
 
-print(junks)
-strips = " ;:,"
-spaces = ('(', ')', '{', '}', '.', '=', '_', '"')
-nothing = ("-", "<", ">", "'", "/", "*", "@", "#", "$")
+strips, spaces, nothing = junks
+strips = strips[6:]
+spaces = spaces[6:]
+nothing = nothing[8:]
 
-def clearText(str1):
-    for n in nothing:
-        str1 = str1.replace(n, '')
-    for s in spaces:
-        str1 = str1.replace(s, ' ')
-    return str1
+def clearText(strLine):
+    for case in nothing:
+        strLine = strLine.replace(case, '')
+    for case in spaces:
+        strLine = strLine.replace(case, ' ')
+    return strLine
 
-def addWords(key):
+def addKeys(key):
     key = key.strip(strips)
-    if key in words:
-        words[key] += 1
-    else:
-        words[key] = 1
+    key = key.lower()
+    if len(key) > 2:
+        if key not in words:
+            words[key] = 1
+        else:
+            words[key] += 1
 
 words = dict()
-with open(filePath, 'r') as f:
+with open(filePath) as f:
     while True:
         line = f.readline()
 
         if line == '':
-            break
+            break  
 
         line = clearText(line)
         line = line.split()
-        for w in line:
-            addWords(w)
+        for word in line:
+            addKeys(word)
 
 print()
-print(words)
+for w in words:
+    print(w, words[w])
