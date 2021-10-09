96cc683abcb721e9cba013111cc5c5879d849424
arya <amirh4liyan@gmail.com>
2021-10-09 18:56:20 +0330

    work with parseArge to make program better :)

--- repeated.py
+++ repeated.py
@@ -1,3 +1,27 @@
+import argparse
+
+# make parser object
+parser = argparse.ArgumentParser()
+
+
+# add arguments here:
+parser.add_argument('x', type=int, help='the base')
+parser.add_argument('y', type=int, help='the exponent')
+parser.add_argument('-v', '--verbosity', action='count', default=0)
+
+
+# get some data from command-line with parse_arge() method
+args = parser.parse_args()
+answer = args.x**args.y
+if args.verbosity >= 2:
+    print('the square of %d equals %d' %(args.x, answer))
+elif args.verbosity >= 1:
+    print('%d^%d = %d' %(args.x, args.y, answer))
+else:
+    print(answer)
+
+
+'''
 # Get text-file path from USER
 print('Enter Absolute Path')
 print('[if text is in current path, jusu enter the name]')
@@ -47,3 +71,4 @@ with open(filePath) as f:
 print()
 for w in words:
     print(w, words[w])
+'''
